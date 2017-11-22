6801cd0d286bb864f16508e7ef5e2674fa9a7354
MichelM279 <mi.mangold89@gmail.com>
2017-11-22 18:51:33 +0100

    secret korrigiert

--- formmail.php
+++ formmail.php
@@ -6,7 +6,7 @@ foreach($_POST as $key=> $val) {
 $fmtResponse= str_replace("<$key>", $val, $fmtResponse);
 $fmtMail= str_replace("<$key>", $val, $fmtMail);
 }
-if ($_POST["access"] == "irregeheim") {
+if ($_POST["access"] == "kontakt-secret-easy") {
 mail($recipient, $_POST["subject"], $fmtMail);
 }
 echo $fmtResponse;
